24df3ad0255cf42e329c8de8bc2f42f41b2e08ea
DavidPonce-dev <60764076+DavidPonce-dev@users.noreply.github.com>
2021-09-30 09:04:57 -0300

    llamando a modulo listaPreguntas desde el store

--- src/app/store/index.js
+++ src/app/store/index.js
@@ -1,15 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import listaPreguntas from './listaPreguntas'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
+    listaPreguntas
   }
 })
---
+++ src/app/store/listaPreguntas.js
@@ -0,0 +1,26 @@
+import { ipcRenderer } from "electron";
+
+export default {
+  namespaced: true,
+  state: {
+    listaPreguntas: [],
+  },
+  getters: {
+    listaPreguntas: ({ listaPreguntas }) => listaPreguntas,
+  },
+  mutations: {
+    setLista(state, newLista) {
+      state.listaPreguntas = newLista;
+    },
+  },
+  actions: {
+    loadLista: async ({ commit }) => {
+      try {
+        const lista = await ipcRenderer.invoke("leerListas");
+        commit("setLista", lista);
+      } catch (error) {
+        throw new Error('Error obteniendo la lista de preguntas!')
+      }
+    },
+  },
+};
