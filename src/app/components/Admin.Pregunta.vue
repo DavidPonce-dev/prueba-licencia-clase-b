7d6d3eb8045fef6574c32e8783cf91927d64bafa
DavidPonce-dev <60764076+DavidPonce-dev@users.noreply.github.com>
2021-09-30 08:42:51 -0300

    creando componente de pregunta

---
+++ src/app/components/Admin.Pregunta.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-card class="ma-2 pa-2">
+    <v-card-title>{{pregunta.pregunta}}</v-card-title>
+  </v-card>
+</template>
+
+<script>
+export default {
+    props: ['pregunta' ]
+}
+</script>
+
+<style>
+</style>
