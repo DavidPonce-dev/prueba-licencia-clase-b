28605e59b9a61593daee48dcc41c8aed0184d228
DavidPonce-dev <60764076+DavidPonce-dev@users.noreply.github.com>
2021-10-01 04:09:52 -0300

    agregando id a la clase

--- src/electron/cuestionario/models/Pregunta.js
+++ src/electron/cuestionario/models/Pregunta.js
@@ -1,11 +1,13 @@
 export default class Pregunta {
     constructor({
+        id,
         pregunta,
         alternativas,
         imagen,
         dosPuntos
     })
     {
+        this.id = id;
         this.pregunta = pregunta;
         this.alternativas = alternativas;
         this.imagen = imagen;
